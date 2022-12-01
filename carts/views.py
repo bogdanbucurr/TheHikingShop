c4ee5c401ab1aedc0286a08e41249fa7e851147e
bogdanbucur <bogdan.bucur1996@yahoo.com>
2022-12-01 22:32:17 +0200

    Finish the paginator and search tab. Starting the Variation

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 
 
 def _cart_id(request):
@@ -14,6 +15,19 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)  # get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key,
+                                                  variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))  # get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
@@ -58,6 +72,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -66,7 +82,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         tax = (2 * total) / 100
         grand_total = total + tax
 
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
     context = {
         'total': total,
